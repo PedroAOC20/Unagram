cf22e9e6f41602a43ba75f2f98ddd8cff254997d
Igor Ulian <igorulian@outlook.com>
2024-12-02 22:45:59 -0300

    feat: add click to copy link

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -152,7 +152,7 @@
 									<div class="flex space-x-2 items-center">
 										<button type="button" class="focus:outline-none Like"><svg class="w-8 h-8 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.6" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg></button>
 										<button type="button" class="focus:outline-none Comment" @click="comment = !comment"><svg class="w-8 h-8 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.6" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path></svg></button>
-										<button type="button" class="focus:outline-none save"><svg class="w-7 h-7 mb-1 ml-1 text-gray-600  z-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.6" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path></svg></button>
+										<button onclick="sharePost({{$post->id}})" class="focus:outline-none save"><svg class="w-7 h-7 mb-1 ml-1 text-gray-600  z-10" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.6" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"></path></svg></button>
 									</div>
 									<div class="flex space-x-2 items-center">
 										<button type="button" class="focus:outline-none Like"><svg class="w-8 h-8 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.6" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"></path></svg></button>
@@ -181,3 +181,14 @@
 							</div>
 						</div>
 					</div>
+
+
+
+<script>
+	async function sharePost(id){
+		text = `post/${id}`
+		await navigator.clipboard.writeText(text)
+
+		alert("Link do post copiado!");
+}
+</script>
